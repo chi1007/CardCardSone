c5b8fdb53f2796795791c6229dc45a3eb18f660f
whelm1999 <165228977+whelm1999@users.noreply.github.com>
2024-05-24 13:24:10 +0800

    test

--- webapp/app.py
+++ webapp/app.py
@@ -3,7 +3,7 @@ from app1 import app1_blueprint
 from rc_app import rc_app_blueprint
 from database import database_blueprint
 from mail import mail_blueprint
-from sign_up import sign_up_blueprint
+
 
 
 app = Flask(__name__)
@@ -12,7 +12,6 @@ app.register_blueprint(app1_blueprint, url_prefix='/')
 app.register_blueprint(rc_app_blueprint, url_prefix='/rc_app')
 app.register_blueprint(database_blueprint, url_prefix='/database')  
 app.register_blueprint(mail_blueprint, url_prefix='/api_contact') #導入contact之路由 
-app.register_blueprint(sign_up_blueprint, url_prefix='/sign_up') 
 
 
 
