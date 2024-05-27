1ce51d3f9c31aa5ae515bf22f62410b5fbce064c
johsuan <itszoooe@gmail.com>
2024-05-27 19:49:55 +0800

    get_data() update url

--- webapp/rc_app.py
+++ webapp/rc_app.py
@@ -44,7 +44,7 @@ def find_closest_cards(user_vector, cards):
 # 定義路由
 @rc_app_blueprint.route("/", methods=["GET", "POST"])
 def index():
-    questions = get_data('http://127.0.0.1:5000/database/questions')
+    questions = get_data('http://flask:5000/database/questions')
     cards = {}
 
     # 處理表單提交
